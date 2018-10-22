1094861d720b83a8ca63a4198e2b0cb1ad66c5b1
sathish-git <satish581993@gmail.com>
2018-10-22 22:01:18 +0530

    added iml files and created first class

---
+++ core/src/main/java/com/explorations/services/CreatePageFromExcelService.java
@@ -0,0 +1,10 @@
+package com.explorations.services;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component (service = CreatePageFromExcelService.class)
+public class CreatePageFromExcelService {
+
+
+
+}

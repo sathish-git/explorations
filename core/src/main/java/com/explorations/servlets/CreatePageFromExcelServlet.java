30a3e7e2a9bdf26417971707385705de639a7011
sathish-git <satish581993@gmail.com>
2018-10-25 16:50:54 +0530

    further refactoring to clean up

--- core/src/main/java/com/explorations/servlets/CreatePageFromExcelServlet.java
+++ core/src/main/java/com/explorations/servlets/CreatePageFromExcelServlet.java
@@ -33,39 +33,49 @@ public class CreatePageFromExcelServlet extends SlingAllMethodsServlet {
 
     private static final String UPLOADED_FILE = "uploadedFile";
 
+    private static final String SUCCESS_MESSAGE = "Page/s created successfully from excel.";
+
+    private static final String FORMAT_FAILURE_MESSAGE = "Format not supported, Please try again with a proper .xls file";
+
+    private static final String PAGE_CREATION_FAILURE_MESSAGE = "Error occurred while trying to create pages using the excel sheet";
+
+    private static final String NOT_MULTIPART_FAILURE_MESSAGE = "The content uploaded is not a multipart content";
+
+    private static final String NO_FILE_FAILURE_MESSAGE = "No proper file is uploaded, please uploaded a .xls file to proceed";
+
     private static final Logger LOGGER = LoggerFactory.getLogger(CreatePageFromExcelServlet.class);
 
     @Override
     protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException {
-        try {
-            boolean isCreatePageFromExcelSuccess;
-            if (FileUpload.isMultipartContent(new ServletRequestContext(request))) {
-                RequestParameter param = request.getRequestParameter(UPLOADED_FILE);
-                isCreatePageFromExcelSuccess = param != null && createPageFromExcel(request, param.getInputStream());
-                if (isCreatePageFromExcelSuccess) {
-                    response.getWriter().append("Page/s successfully created from excel!").flush();
-                } else response.getWriter().append("Cannot create pages from excel, please try again").flush();
-            } else response.getWriter().append("! The form is not a multipart form.");
-        } catch (NullPointerException | WCMException | BiffException e) {
-            LOGGER.error("An Exception occurred while trying to create pages from excel", e);
-            response.getWriter().print("Error occurred while creating pages from excel");
-        }
+        if (FileUpload.isMultipartContent(new ServletRequestContext(request))) {
+            RequestParameter param = request.getRequestParameter(UPLOADED_FILE);
+            if (param != null) {
+                createPageFromExcel(request, param.getInputStream(), response);
+            } else response.getWriter().append(NO_FILE_FAILURE_MESSAGE).flush();
+        } else response.getWriter().append(NOT_MULTIPART_FAILURE_MESSAGE).flush();
     }
 
-    private boolean createPageFromExcel(SlingHttpServletRequest request, InputStream inputStream) throws WCMException, IOException, BiffException {
-        Workbook workbook = inputStream != null ? Workbook.getWorkbook(inputStream) : null;
-        PageManager pageManager = request.getResourceResolver().adaptTo(PageManager.class);
-        if (workbook != null && pageManager != null) {
-            Sheet sheet = workbook.getSheet(0);
-            for (int i = 1; i < sheet.getRows(); i++) {
-                pageManager.create(
-                        sheet.getCell(0, i).getContents(),
-                        sheet.getCell(1, i).getContents(),
-                        sheet.getCell(2, i).getContents(),
-                        sheet.getCell(3, i).getContents(), true);
+    private void createPageFromExcel(SlingHttpServletRequest request, InputStream inputStream, SlingHttpServletResponse response) throws IOException {
+        try {
+            Workbook workbook = inputStream != null ? Workbook.getWorkbook(inputStream) : null;
+            PageManager pageManager = request.getResourceResolver().adaptTo(PageManager.class);
+            if (workbook != null && pageManager != null) {
+                Sheet sheet = workbook.getSheet(0);
+                for (int i = 1; i < sheet.getRows(); i++) {
+                    pageManager.create(
+                            sheet.getCell(0, i).getContents(),
+                            sheet.getCell(1, i).getContents(),
+                            sheet.getCell(2, i).getContents(),
+                            sheet.getCell(3, i).getContents(), true);
+                }
+                response.getWriter().append(SUCCESS_MESSAGE).flush();
             }
-            return true;
+        } catch (BiffException e) {
+            LOGGER.error("Encountered a Biff exception while using extracting a workbook", e);
+            response.getWriter().append(FORMAT_FAILURE_MESSAGE).flush();
+        } catch (WCMException e) {
+            LOGGER.error("Encountered an exception while creating a page using PageManager", e);
+            response.getWriter().append(PAGE_CREATION_FAILURE_MESSAGE).flush();
         }
-        return false;
     }
 }

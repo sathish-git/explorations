703088fdb20cd56c47da7e903f832ec570a92b9a
sathish-git <satish581993@gmail.com>
2018-10-25 16:10:15 +0530

    Removed all service, bean and model classes. Replaced all with a single servlet and added direct upload of excel.

--- core/src/main/java/com/explorations/servlets/CreatePageFromExcelServlet.java
+++ core/src/main/java/com/explorations/servlets/CreatePageFromExcelServlet.java
@@ -1,18 +1,23 @@
 package com.explorations.servlets;
 
+import com.day.cq.wcm.api.PageManager;
 import com.day.cq.wcm.api.WCMException;
-import com.explorations.services.CreatePageFromExcelService;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+import org.apache.commons.fileupload.FileUpload;
+import org.apache.commons.fileupload.servlet.ServletRequestContext;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.request.RequestParameter;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
-import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.Servlet;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Component(
         service = Servlet.class,
@@ -26,26 +31,41 @@ import java.io.IOException;
 )
 public class CreatePageFromExcelServlet extends SlingAllMethodsServlet {
 
-    private static final String SELECTED_EXCEL = "selectedExcel";
+    private static final String UPLOADED_FILE = "uploadedFile";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CreatePageFromExcelServlet.class);
-
-    @Reference
-    private CreatePageFromExcelService createPageFromExcelService;
 
     @Override
     protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException {
         try {
-            String selectedExcelResourcePath = request.getParameter(SELECTED_EXCEL);
-            if (createPageFromExcelService.createPageFromExcel(request, selectedExcelResourcePath)) {
-                response.getWriter().append("Page/Pages created successfully!").flush();
-            }
-            response.getWriter().append("Failed creating pages from excel").flush();
-
-        } catch (WCMException e) {
+            boolean isCreatePageFromExcelSuccess;
+            if (FileUpload.isMultipartContent(new ServletRequestContext(request))) {
+                RequestParameter param = request.getRequestParameter(UPLOADED_FILE);
+                isCreatePageFromExcelSuccess = param != null && createPageFromExcel(request, param.getInputStream());
+                if (isCreatePageFromExcelSuccess) {
+                    response.getWriter().append("Page/s successfully created from excel!").flush();
+                } else response.getWriter().append("Cannot create pages from excel, please try again").flush();
+            } else response.getWriter().append("! The form is not a multipart form.");
+        } catch (NullPointerException | WCMException | BiffException e) {
             LOGGER.error("An Exception occurred while trying to create pages from excel", e);
             response.getWriter().print("Error occurred while creating pages from excel");
         }
     }
 
+    private boolean createPageFromExcel(SlingHttpServletRequest request, InputStream inputStream) throws WCMException, IOException, BiffException {
+        Workbook workbook = inputStream != null ? Workbook.getWorkbook(inputStream) : null;
+        PageManager pageManager = request.getResourceResolver().adaptTo(PageManager.class);
+        if (workbook != null && pageManager != null) {
+            Sheet sheet = workbook.getSheet(0);
+            for (int i = 1; i < sheet.getRows(); i++) {
+                pageManager.create(
+                        sheet.getCell(0, i).getContents(),
+                        sheet.getCell(1, i).getContents(),
+                        sheet.getCell(2, i).getContents(),
+                        sheet.getCell(3, i).getContents(), true);
+            }
+            return true;
+        }
+        return false;
+    }
 }
